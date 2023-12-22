8970e245fdbd876afbf5abb1988a2e41ea8e96ed
nuwan-dev <nuwanc.email@gmail.com>
2023-12-22 16:40:39 +0530

    mb-nav-icon-change

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,5 +1,7 @@
 const tglBtn = document.querySelector(".mb-tp-nav-tgl-btn");
 const mbNav = document.querySelector(".mb-nav");
+
 tglBtn.addEventListener("click", () => {
+  tglBtn.classList.toggle("mb-tgl-btn-active");
   mbNav.classList.toggle("mb-nav-active");
 });

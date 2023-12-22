0efa2e0bd8e2f3650f79562f92a149e42880f738
nuwan-dev <nuwanc.email@gmail.com>
2023-12-22 21:43:31 +0530

    mobile nav scrool, hero mobile view, mobile nav backgrond

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,7 +1,13 @@
 const tglBtn = document.querySelector(".mb-tp-nav-tgl-btn");
 const mbNav = document.querySelector(".mb-nav");
-
 tglBtn.addEventListener("click", () => {
   tglBtn.classList.toggle("mb-tgl-btn-active");
   mbNav.classList.toggle("mb-nav-active");
+});
+
+const topNav = document.querySelector(".gym-mb-top-nav-container");
+window.addEventListener("scroll", () => {
+  window.scrollY > 1
+    ? topNav.classList.add("mb-nav-scroll")
+    : topNav.classList.remove("mb-nav-scroll");
 });

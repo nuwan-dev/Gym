6ffc965e12af887c526205fc4285a8eba3ef8d26
nuwan-dev <nuwanc.email@gmail.com>
2023-12-30 22:34:11 +0530

    c

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,13 +1,19 @@
 // mobile nav bar
 const tglBtn = document.querySelector(".mb-tp-nav-tgl-btn");
 const mbNav = document.querySelector(".mb-nav");
-const topNav = document.querySelector(".gym-mb-top-nav-container");
-
 tglBtn.addEventListener("click", () => {
   tglBtn.classList.toggle("mb-tgl-btn-active");
   mbNav.classList.toggle("mb-nav-active");
 });
+
+const topNav = document.querySelector(".gym-mb-top-nav-container");
 window.addEventListener("scroll", () => {
+  // if (window.scrollY > 1) {
+  //   topNav.classList.add("mb-nav-scroll");
+  // } else {
+  //   topNav.classList.remove("mb-nav-scroll");
+  // }
+
   window.scrollY > 1
     ? topNav.classList.add("mb-nav-scroll")
     : topNav.classList.remove("mb-nav-scroll");
